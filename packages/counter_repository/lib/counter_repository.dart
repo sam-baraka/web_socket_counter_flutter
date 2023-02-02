3934261c31a587d860e8ec2ca5ef65b935250507
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-02 10:20:02 +0300

    feat: Added counter repository package

---
+++ packages/counter_repository/lib/counter_repository.dart
@@ -0,0 +1,4 @@
+/// A Dart package which manages the counter domain.
+library counter_repository;
+
+export 'src/counter_repository.dart';
---
+++ packages/counter_repository/lib/src/counter_repository.dart
@@ -0,0 +1,7 @@
+/// {@template counter_repository}
+/// A Dart package which manages the counter domain.
+/// {@endtemplate}
+class CounterRepository {
+  /// {@macro counter_repository}
+  const CounterRepository();
+}
